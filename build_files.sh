538e8a7680d7fe7f9ed9af6ca655aaf958e3da05
Sumit <vermasumit923@gmail.com>
2023-08-27 00:45:29 +0530

    trying to avoid warinings

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
 pip install --upgrade pip
-pip install --root-user-action=ignore -r requirements.txt
+pip install --root-user-action=ignore requests
+pip install -r requirements.txt
 python3.9 manage.py collectstatic
 echo "BUILD END"
