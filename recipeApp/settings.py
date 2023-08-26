d993530459b011d8c38237b54e35eb10fb7e95b5
Sumit <vermasumit923@gmail.com>
2023-08-27 01:05:31 +0530

    changed settings.py

--- recipeApp/settings.py
+++ recipeApp/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dk26fyzkl',
