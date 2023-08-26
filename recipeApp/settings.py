93a0845274c91d7a2d95865dca07f8365449a028
Sumit <vermasumit923@gmail.com>
2023-08-27 01:01:00 +0530

    added static files url

--- recipeApp/settings.py
+++ recipeApp/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dk26fyzkl',
