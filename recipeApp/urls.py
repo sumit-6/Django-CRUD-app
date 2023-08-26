d86919ee874f25046021a80daa01e806dee058bf
Sumit <vermasumit923@gmail.com>
2023-08-26 23:20:16 +0530

    settings.py changes

--- recipeApp/urls.py
+++ recipeApp/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
     
