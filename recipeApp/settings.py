1b082aedf272fb82926e83a97d279b416fb179ef
Sumit <vermasumit923@gmail.com>
2023-08-26 20:49:05 +0530

    changed media_url

--- recipeApp/settings.py
+++ recipeApp/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
 ]
 
 EXTERNAL_APPS = [
@@ -134,8 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIR = os.path.join(BASE_DIR, "static")
 
 
-MEDIA_ROOT = BASE_DIR/'media'
-MEDIA_URL = 'img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "public/static")
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
